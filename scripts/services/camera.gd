f8e17a13d6829c7bc2dec6248ad6171c1d4ee2e3
CzlowiekImadlo <quak@tlen.pl>
2016-01-30 16:56:20 +0100

    Camera service that follows players

---
+++ scripts/services/camera.gd
@@ -0,0 +1,41 @@
+
+var bag
+
+var camera = preload("res://scenes/camera.tscn").instance()
+
+const CAMERA_FIXED_Y = 100
+
+func _init_bag(bag):
+    self.bag = bag
+
+func attach():
+    self.bag.board.attach(self.camera)
+    self.bag.processing.register(self)
+
+func detach():
+    self.bag.board.detach(self.camera)
+    self.bag.processing.remove(self)
+
+func get_pos():
+    return self.camera.get_pos()
+
+func set_pos(position):
+    self.camera.set_pos(position)
+
+func process(delta):
+    var average_position = Vector2(0, 0)
+    var count = 0
+
+    var camera_position = self.camera.get_pos()
+    var player_position
+
+    for player in self.bag.players.players:
+        player_position = player.get_pos()
+        average_position = average_position + player_position
+        count = count + 1
+
+    if count > 0:
+        average_position = average_position / count
+        average_position.y = self.CAMERA_FIXED_Y
+        if average_position.x > camera_position.x:
+            self.camera.set_pos(average_position)
--- scripts/services/dependency_bag.gd
+++ scripts/services/dependency_bag.gd
@@ -10,6 +10,7 @@ var board = preload("res://scripts/services/board.gd").new()
 var sound = preload("res://scripts/controllers/sound_controller.gd").new()
 var action = preload("res://scripts/controllers/action_controller.gd").new()
 var maps = preload("res://scripts/services/maps.gd").new()
+var camera = preload("res://scripts/services/camera.gd").new()
 
 func _init(root_node):
     self.root = root_node
@@ -21,3 +22,4 @@ func _init(root_node):
     self.sound._init_bag(self, root_node)
     self.action._init_bag(self)
     self.maps._init_bag(self)
+    self.camera._init_bag(self)
