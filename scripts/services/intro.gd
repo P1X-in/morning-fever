152222e45f68b6dbf98e80b140d22c9bc0b5ada2
WermooZ <p.weremczuk@gmail.com>
2016-01-30 17:38:44 +0100

    random subtitles

--- scripts/services/intro.gd
+++ scripts/services/intro.gd
@@ -1,4 +1,14 @@
 extends "res://scripts/services/abstract_screen.gd"
 
+var subtitle
+var subtitles = ['city patrol', 'strikes again!', 'my local bum', 'recycler rush', 'menel patrol', 'unimaginable thirst', 'psycho wizard tactics', 'not so self flowing flask', 'this can of yours']
+
 func _init():
     self.screen_scene = preload("res://scenes/branding/title.tscn").instance()
+    self.subtitle = self.screen_scene.get_node('center/subtitle')
+    self.subtitle.set_text(self.generate_subtitle())
+
+func generate_subtitle():
+    randomize()
+    var number = randi() % self.subtitles.size()
+    return self.subtitles[number]
