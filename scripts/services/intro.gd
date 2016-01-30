29311ebe8f780930f001c7339acd36cb731ca95c
WermooZ <p.weremczuk@gmail.com>
2016-01-30 21:48:16 +0100

    disable subtitle changes when not in intro

--- scripts/services/intro.gd
+++ scripts/services/intro.gd
@@ -2,6 +2,7 @@ extends "res://scripts/services/abstract_screen.gd"
 
 var subtitle
 var subtitles = ['city patrol', 'strikes again!', 'my local bum', 'recycler rush', 'menel overwatch', 'unimaginable thirst', 'psycho wizard tactics', 'not so self flowing flask', 'this can of yours', 'hobocraft', 'suburban infiltrator']
+var is_detached = false
 
 func _init():
     self.screen_scene = preload("res://scenes/branding/title.tscn").instance()
@@ -15,5 +16,13 @@ func _init_bag(bag, container):
 func set_subtitle():
     var number = randi() % self.subtitles.size()
     self.subtitle.set_text(self.subtitles[number])
+    if !self.is_detached:
+        self.bag.timers.set_timeout(5, self, "set_subtitle")
 
-    self.bag.timers.set_timeout(5, self, "set_subtitle")
+func attach():
+    .attach()
+    self.is_detached = false
+
+func detach():
+    self.is_detached = true
+    .detach()
