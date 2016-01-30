dc779e1d60d6669307ce305318473efd4bbb09b7
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2016-01-30 20:11:07 +0100

    cola + ysort

--- scripts/services/board.gd
+++ scripts/services/board.gd
@@ -30,7 +30,7 @@ func attach_object(object):
         return
 
     self.attached_objects[object.get_instance_ID()] = object
-    self.current_map.add_child(object)
+    self.current_map.get_node('ysort').add_child(object)
 
 func detach_object(object):
     if self.current_map == null or not self.mount.is_a_parent_of(self.current_map) or not self.current_map.is_a_parent_of(object):
