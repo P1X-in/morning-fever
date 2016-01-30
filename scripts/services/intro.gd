058d4bf2d704b58e10c18cf28e28f1d8602d1041
WermooZ <p.weremczuk@gmail.com>
2016-01-30 18:14:01 +0100

    random subtitles timer

--- scripts/services/intro.gd
+++ scripts/services/intro.gd
@@ -1,14 +1,19 @@
 extends "res://scripts/services/abstract_screen.gd"
 
 var subtitle
-var subtitles = ['city patrol', 'strikes again!', 'my local bum', 'recycler rush', 'menel patrol', 'unimaginable thirst', 'psycho wizard tactics', 'not so self flowing flask', 'this can of yours']
+var subtitles = ['city patrol', 'strikes again!', 'my local bum', 'recycler rush', 'menel overwatch', 'unimaginable thirst', 'psycho wizard tactics', 'not so self flowing flask', 'this can of yours', 'hobocraft', 'suburban infiltrator']
 
 func _init():
     self.screen_scene = preload("res://scenes/branding/title.tscn").instance()
     self.subtitle = self.screen_scene.get_node('center/subtitle')
-    self.subtitle.set_text(self.generate_subtitle())
 
-func generate_subtitle():
+func _init_bag(bag, container):
+    ._init_bag(bag, container)
     randomize()
+    self.set_subtitle()
+
+func set_subtitle():
     var number = randi() % self.subtitles.size()
-    return self.subtitles[number]
+    self.subtitle.set_text(self.subtitles[number])
+
+    self.bag.timers.set_timeout(5, self, "set_subtitle")
