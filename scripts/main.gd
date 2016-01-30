303c0ffb9257f2510433a5d5147e2d4f66798a23
CzlowiekImadlo <quak@tlen.pl>
2016-01-30 14:14:22 +0100

    Changed mount point for screens

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,8 @@ var bag
 var mount
 
 func _ready():
-    self.mount = self.get_node("/root/game")
+    self.mount = self.get_node("/root/game/mount")
+
     self.bag = preload("res://scripts/services/dependency_bag.gd").new(self)
     self.set_process_input(true)
     self.set_fixed_process(true)
