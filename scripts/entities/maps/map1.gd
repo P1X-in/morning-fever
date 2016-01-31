66692b385fa7ca069ea964731302a4b7600efb69
WermooZ <p.weremczuk@gmail.com>
2016-01-31 14:37:23 +0100

    more stages

--- scripts/entities/maps/map1.gd
+++ scripts/entities/maps/map1.gd
@@ -11,4 +11,11 @@ func _init():
         { 'distance': 250, 'waves': [2, 3, 3] },
         { 'distance': 350, 'waves': [2, 3, 3] },
         { 'distance': 450, 'waves': [3, 4, 4] },
+        { 'distance': 550, 'waves': [4, 5, 5] },
+        { 'distance': 650, 'waves': [4, 5, 5] },
+        { 'distance': 850, 'waves': [5, 5, 6] },
+        { 'distance': 1050, 'waves': [6, 3, 5] },
+        { 'distance': 1350, 'waves': [5, 3, 5 , 4] },
+        { 'distance': 1550, 'waves': [6, 6 ,7] },
+        { 'distance': 1950, 'waves': [4, 5, 6, 7, 8, 10] }
     ]
