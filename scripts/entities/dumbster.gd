40c3bbd7db790217492795e58fc0c73a165caf21
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2016-01-31 12:50:47 +0100

    map

--- scripts/entities/dumbster.gd
+++ scripts/entities/dumbster.gd
@@ -1,18 +1,16 @@
 
 var bag
 var size = 7
-var body
 
 func _ready():
     self.bag = self.get_node('/root/game').bag
-    self.body = self.get_node('body')
     self.register()
 
 func register():
     self.bag.items.add(self)
 
 func hit():
-    self.body.set_frame(1)
+    print('got hit')
 
 func reset():
-    self.body.set_frame(0)
+    return
