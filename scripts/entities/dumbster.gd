7f4e2284ce697d8c8f1b66e51de9694be2e45a9f
CzlowiekImadlo <quak@tlen.pl>
2016-01-31 12:33:33 +0100

    Opening dumpsters

--- scripts/entities/dumbster.gd
+++ scripts/entities/dumbster.gd
@@ -1,16 +1,18 @@
 
 var bag
 var size = 7
+var body
 
 func _ready():
     self.bag = self.get_node('/root/game').bag
+    self.body = self.get_node('body')
     self.register()
 
 func register():
     self.bag.items.add(self)
 
 func hit():
-    print('got hit')
+    self.body.set_frame(1)
 
 func reset():
-    return
+    self.body.set_frame(0)
